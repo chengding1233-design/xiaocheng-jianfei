1fa11368869d8e47f9118147aa91a903facb623d
chengding1233-design <chengding.1233@gmail.com>
2026-09-06 01:15:55 +0800

    Deploy static site to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = "jianfei-app-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/manifest.webmanifest",
+  "./assets/icon.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      const fetched = fetch(e.request)
+        .then((res) => {
+          const cp = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, cp));
+          return res;
+        })
+        .catch(() => hit);
+      return hit || fetched;
+    })
+  );
+});
